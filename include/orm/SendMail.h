eb931ff85cfc5836ec8ec2b4bc5bfa151d3432d5
Zhuravlev Nikolay <kolyansp22222@mail.ru>
2021-05-08 12:29:19 +0300

    bug fixed

--- include/orm/SendMail.h
+++ include/orm/SendMail.h
@@ -19,7 +19,7 @@ public:
         head_mail(_head_mail),
         body_mail(_body_mail),
         time_send(_time_send) {}
-    static void DeleteFromQueue(SQLWrapper &db, int id);
+    static void DeleteFromQueue(SQLWrapper &db);
     static std::vector<SendMail> NeedSend(SQLWrapper &db);
     static int AddInQueue(SQLWrapper &db, const std::string &recipient, const std::string &head_mail,
             const std::string &body_mail, time_t time_send);
