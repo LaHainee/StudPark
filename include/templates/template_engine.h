6399258917f913dde3b4ad512b420d604f0df1da
Виталий <vitalyerhsov2001@gmail.com>
2021-05-25 19:05:29 +0300

    Fixes and changes

--- include/templates/template_engine.h
+++ include/templates/template_engine.h
@@ -18,16 +18,16 @@ using json = nlohmann::json;
 class TemplateEngine {
 public:
     std::string RenderTemplate(const json& data, const std::string& templateToParse);
-    std::string RenderPosts(SQLWrapper &wrapper, int groupId, bool isLeader, bool isAuthenticated, std::string user);
+    std::string RenderPosts(SQLWrapper &wrapper, int groupId, bool isLeader=false, bool isAuthenticated=false, std::string user="");
     std::string RenderProfile(SQLWrapper &wrapper, const std::vector<Contact> &contacts,
-                                              const Student &student, bool isAuthenticated, std::string user);
+                                              const Student &student, bool isAuthenticated=false, std::string user="");
     std::string RenderSettings(SQLWrapper &wrapper, const std::vector<Contact> &contacts,
-                                               const Student &student, bool isAuthenticated, std::string user);
+                                               const Student &student, bool isAuthenticated=false, std::string user="");
     std::string RenderErrors(const std::string &error, bool isAuthenticated, std::string user);
-    std::string RenderGroupList(SQLWrapper &wrapper, int groupId, bool isAuthenticated, std::string user);
-    std::string RenderLoginPage(bool isAuthenticated, std::string user);
-    std::string RenderAdminPage(bool isAuthenticated, std::string user);
-    std::string RenderSignupPage(bool isAuthenticated, std::string user);
+    std::string RenderGroupList(SQLWrapper &wrapper, int groupId, bool isAuthenticated=false, std::string user="");
+    std::string RenderLoginPage(bool isAuthenticated=false, std::string user="");
+    std::string RenderAdminPage(bool isAuthenticated=false, std::string user="");
+    std::string RenderSignupPage(bool isAuthenticated=false, std::string user="");
 private:
     Environment env;
 };
