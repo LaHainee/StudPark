84b46c7c495918d3a5e163d3b6d9dad5491f7c84
Leonid <belyeavleonid2000@mail.ru>
2021-06-03 09:27:30 +0300

    Edited fields

--- templates/js/upload.js
+++ templates/js/upload.js
@@ -24,11 +24,13 @@ function randstr() {
 }
 
 function uploadFile() {
+  var btnupload = document.getElementById('btn-upload');
   var fileupload = document.getElementById("fileupload");
   var files = fileupload.files;
   if (!files.length) {
     return alert("Сначала выберите файл.");
   }
+  btnupload.disabled = true;
   var file = files[0];
   var fileName = file.name;
   var albumPhotosKey = encodeURIComponent(albumName) + "/";
@@ -53,9 +55,11 @@ function uploadFile() {
       document.getElementById('body').value += `\nСсылка: <a href=${data['Location']}>${fileName}</a>`;
       fileupload.value = "";
       alert("Файл успешно загружен в облако");
+      btnupload.disabled = false;
     },
     function(err) {
       return alert("Произошла ошибка при загрузке файла: ", err.message);
+      btnupload.disabled = false;
     }
   );
 }
